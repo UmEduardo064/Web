7012c0b63db784bc802b50e4d1cf2d45134defa1
UmEduardo064 <eduardosalespf064@gmail.com>
2025-03-26 19:16:02 -0300

    Removed

--- Jogoguinho/TheGame.js
+++
@@ -1,9 +0,0 @@
-function randomNumber() {
-    const rn = Math.floor(Math.random() * 200) + 1;
-
-// Math.random():  Gera um número decimal aleatório entre 0 (inclusive) e 1 (exclusive).
-// Multiplicar por 200:  gera um número entre 0 (inclusive) e 200 (exclusive).
-// Math.floor():  Arredonda o número para baixo, removendo a parte decimal.
-// Adicionar + 1:  Ajusta o intervalo para 1 a 200 (em vez de 0 a 199).
-}
-
