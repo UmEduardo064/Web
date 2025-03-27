1fe2e2db2f2b75c74b61815f89f76bd0d90e0626
UmEduardo064 <eduardosalespf064@gmail.com>
2025-03-27 16:09:48 -0300

    Upadate

--- Game/TheGame.js
+++ Game/TheGame.js
@@ -1,29 +1,33 @@
+const ran = Math.floor(Math.random() * 200) + 1;
+console.log("Número secreto (para teste):", ran);
+
+function verification() {
+    const numberUser = parseInt(document.getElementById('numero').value);
+    const mensDica = document.getElementById('maiormenor');
+    const mensResultado = document.getElementById('resultado');
+
+    // Limpar mensagens anteriores
+    mensDica.textContent = '';
+    mensResultado.textContent = '';
+
+    if (ran === numberUser) {
+        mensResultado.textContent = 'Acertou';
+        mensResultado.style.color = 'green';
+        mensDica.textContent = "";    
+    } else if (ran < numberUser) {
+        mensResultado.textContent = "Tente Novamente!";
+        mensResultado.style.color = 'red';
+        mensDica.textContent = "O numero e menor que --> " + numberUser;
+        mensDica.style.color = 'yellow';
+    } else {
+        mensResultado.textContent = "Tente Novamente!";
+        mensResultado.style.color = 'red';
+        mensDica.textContent = "O numero e maior que --> " + numberUser;
+        mensDica.style.color = 'yellow';
+    }
+}
+
 // Math.random():  Gera um número decimal aleatório entre 0 (inclusive) e 1 (exclusive).
 // Multiplicar por 200:  gera um número entre 0 (inclusive) e 200 (exclusive).
 // Math.floor():  Arredonda o número para baixo, removendo a parte decimal.
 // Adicionar + 1:  Ajusta o intervalo para 1 a 200 (em vez de 0 a 199).
-
-const ran = Math.floor(Math.random() * 200) + 1;
-
-
-function verification() {
-
-const numberUser = document.getElementById('numero').value;
-const mensMaiorMenor = document.getElementById('maiormenor');
-const mensCorreto = document.getElementById('resut');
-
-// Limpar mensagens anteriores
-mensMaiorMenor.textContent = '';
-mensCorreto.textContent = '';
-
-if( ran == numberUser){
-    mensCorreto.textContent = 'Acertou';
-    mensCorreto.style.color = 'green';    
-}else if(ran < numberUser) {
-    mensCorreto.textContent = "numero maior"
-    mensCorreto.style.color = 'red';
-}else if(ran > numberUser){
-    mensCorreto.textContent = "numero menor"
-    mensCorreto.style.color = 'red';
-}
-}
